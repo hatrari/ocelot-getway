04b10f1ffc87253886ba947637eda83792bcc048
home <ham.atrari@gmail.com>
2020-11-07 10:56:51 +0100

    add customers controller and tips file

---
+++ customers/Controllers/CustomersController.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace customers.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class CustomersController : ControllerBase
+	{
+		private readonly string[] customers = new[] {"Customer 1", "Customer 2"};
+		[HttpGet]
+		public IEnumerable<string> Get()
+		{
+			return customers;
+		}
+	}
+}
